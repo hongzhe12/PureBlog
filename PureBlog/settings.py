f20919796a77820e3681fd756f5f815c4317dc3f
hzml <hongzhe2022@163.com>
2024-08-17 18:43:24 +0800

    更新配置

--- PureBlog/settings.py
+++ PureBlog/settings.py
@@ -86,11 +86,11 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['DB_NAME'],
-            'HOST': os.environ['DB_HOST'],
-            'PORT': os.environ['DB_PORT'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASSWORD']
+            'NAME': 'dev',
+            'HOST': 'postgres',
+            'PORT': 5432,
+            'USER': 'dev',
+            'PASSWORD': 'Hhz520123'
         }
     }
 
