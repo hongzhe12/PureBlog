39476ca94af68a4222972cb4c8c49a584228f608
hzml <hongzhe2022@163.com>
2024-08-17 17:28:04 +0800

    修复默认DEBUG=False

--- PureBlog/settings.py
+++ PureBlog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--jf5@)96zkz3arz#5(-^%t5cq-q0udrnxw!8dil5f!#%tdsbq%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
